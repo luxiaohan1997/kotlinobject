f11cac4abccaf159d63c0367dfcc95300c56698a
luxiaohan1997 <843134192@qq.com>
2019-10-29 17:07:42 +0800

    实现切换

--- app/src/main/java/com/goketech/smartcommunity/view/my/MyFragment.kt
+++ app/src/main/java/com/goketech/smartcommunity/view/my/MyFragment.kt
@@ -1,27 +1,26 @@
 package com.goketech.smartcommunity.view.my
 
 import android.view.View
+import com.goketech.smartcommunity.R
 import com.goketech.smartcommunity.base.BaseFragment
 import com.goketech.smartcommunity.interfaces.my.MyConstact
+import com.goketech.smartcommunity.presenter.my.MyPresenter
 
 class MyFragment :BaseFragment<MyConstact.View,MyConstact.Presenter>(),MyConstact.View{
     override val layout: Int
-        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+        get() = R.layout.fragment_my  //To change initializer of created properties use File | Settings | File Templates.
 
     override fun initView(view: View) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun initData() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun initPresenter(): MyConstact.Presenter {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return MyPresenter()
     }
 
     override fun onError(err: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
 }
--- app/src/main/java/com/goketech/smartcommunity/view/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/goketech/smartcommunity/view/notifications/NotificationsFragment.kt
@@ -1,28 +1,27 @@
 package com.goketech.smartcommunity.view.notifications
 
 import android.view.View
+import com.goketech.smartcommunity.R
 import com.goketech.smartcommunity.base.BaseFragment
 import com.goketech.smartcommunity.interfaces.notifications.NotificationsConstact
+import com.goketech.smartcommunity.presenter.notifications.NotificationsPresenter
 
 class NotificationsFragment: BaseFragment<NotificationsConstact.View, NotificationsConstact.Presenter>(),
     NotificationsConstact.View{
     override val layout: Int
-        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+        get() = R.layout.fragment_notifcations //To change initializer of created properties use File | Settings | File Templates.
 
     override fun initView(view: View) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun initData() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun initPresenter(): NotificationsConstact.Presenter {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return NotificationsPresenter()
     }
 
     override fun onError(err: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
 }
